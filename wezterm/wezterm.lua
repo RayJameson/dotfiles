f53b627ff10c1ca454a3480378680e46f46b867f
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-06-19 00:38:22 +0600

    feat(wezterm): add tmux like keymaps
    took reference from here
    https://github.com/prabirshrestha/dotfiles/blob/79fb6d5e68f1be32404b2b99eda33e04e240b586/.config/wezterm/wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 local config = {}
 -- In newer versions of wezterm, use the config_builder which will
@@ -7,6 +8,7 @@ local config = {}
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
+local enable_tmux_like_keymaps = true
 
 -- This is where you actually apply your config choices
 config.window_background_opacity = 0.7
@@ -16,10 +18,13 @@ if wezterm.target_triple:match("darwin") then
   config.macos_window_background_blur = 30
 elseif wezterm.target_triple:match("windows") then
   config.win32_system_backdrop = "Acrylic"
+  config.term = "" -- Set to empty so FZF works on windows
+  config.default_prog = { "cmd.exe" }
 end
 
 -- config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
+config.tab_max_width = 25
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
 
@@ -50,9 +55,6 @@ config.visual_bell = {
   fade_out_function = "EaseOut",
   fade_out_duration_ms = 50,
 }
-
-local act = wezterm.action
-
 config.keys = {
   -- Rebind CTRL-SHIFT <-/-> to match TMUX keybindings
   {
@@ -72,6 +74,113 @@ config.keys = {
     },
   },
 }
+
+if enable_tmux_like_keymaps then
+  config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 50000 }
+  local tmux_keymap = {
+    -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+    {
+      key = "a",
+      mods = "LEADER|CTRL",
+      action = act.SendKey { key = "a", mods = "CTRL" },
+    },
+    {
+      key = '"',
+      mods = "LEADER",
+      action = act.SplitVertical { domain = "CurrentPaneDomain" },
+    },
+    {
+      key = "%",
+      mods = "LEADER",
+      action = act.SplitHorizontal { domain = "CurrentPaneDomain" },
+    },
+    {
+      key = "-",
+      mods = "LEADER",
+      action = act.SplitVertical { domain = "CurrentPaneDomain" },
+    },
+    {
+      key = "|",
+      mods = "LEADER",
+      action = act.SplitHorizontal { domain = "CurrentPaneDomain" },
+    },
+    { key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+    {
+      key = "c",
+      mods = "LEADER",
+      action = act.SpawnTab("CurrentPaneDomain"),
+    },
+    {
+      key = "h",
+      mods = "CTRL",
+      action = act.ActivatePaneDirection("Left"),
+    },
+    {
+      key = "j",
+      mods = "CTRL",
+      action = act.ActivatePaneDirection("Down"),
+    },
+    {
+      key = "k",
+      mods = "CTRL",
+      action = act.ActivatePaneDirection("Up"),
+    },
+    {
+      key = "l",
+      mods = "CTRL",
+      action = act.ActivatePaneDirection("Right"),
+    },
+    {
+      key = "H",
+      mods = "LEADER",
+      action = act.AdjustPaneSize { "Left", 5 },
+    },
+    {
+      key = "J",
+      mods = "LEADER",
+      action = act.AdjustPaneSize { "Down", 5 },
+    },
+    {
+      key = "K",
+      mods = "LEADER",
+      action = act.AdjustPaneSize { "Up", 5 },
+    },
+    {
+      key = "L",
+      mods = "LEADER",
+      action = act.AdjustPaneSize { "Right", 5 },
+    },
+    {
+      key = "&",
+      mods = "LEADER",
+      action = act.CloseCurrentTab { confirm = true },
+    },
+    {
+      key = "x",
+      mods = "LEADER",
+      action = act.CloseCurrentPane { confirm = true },
+    },
+    {
+      key = "p",
+      mods = "LEADER",
+      action = act.ActivateTabRelative(-1),
+    },
+    {
+      key = "n",
+      mods = "LEADER",
+      action = act.ActivateTabRelative(1),
+    },
+  }
+  for i = 1, 9 do
+    table.insert(
+      config.keys,
+      { key = tostring(i), mods = "LEADER", action = act.ActivateTab(i - 1) }
+    )
+  end
+  for i = 1, #tmux_keymap do
+    table.insert(config.keys, tmux_keymap[i])
+  end
+end
 
 -- and finally, return the configuration to wezterm
 return config
