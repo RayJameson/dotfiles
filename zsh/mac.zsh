b69759f40a564d9e611213d9dbdb5a4c4114b866
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-09-02 03:17:59 +0600

    feat(maczshrc): add ORACLE_HOME

--- zsh/mac.zsh
+++ zsh/mac.zsh
@@ -1,5 +1,6 @@
 export PNPM_HOME="$HOME/Library/pnpm"
-export PATH="opt/homebrew/Cellar/pyenv-virtualenv/1.1.5/shims:/opt/homebrew/bin:/opt/homebrew/sbin:/Library/Frameworks/Python.framework/Versions/3.10/bin:/Library/Frameworks/Python.framework/Versions/3.9/bin:/Library/Apple/usr/bin:/usr/local/opt/fzf/bin:$HOME/.gem/ruby/2.6.0/bin:$PNPM_HOME:$PATH"
+export ORACLE_HOME="/opt/oracle/instantclient_18_1"
+export PATH="opt/homebrew/Cellar/pyenv-virtualenv/1.1.5/shims:/opt/homebrew/bin:/opt/homebrew/sbin:/Library/Frameworks/Python.framework/Versions/3.10/bin:/Library/Frameworks/Python.framework/Versions/3.9/bin:/Library/Apple/usr/bin:/usr/local/opt/fzf/bin:$HOME/.gem/ruby/2.6.0/bin:$PNPM_HOME:$ORACLE_HOME:$PATH"
 export PATH="/opt/homebrew/opt/man-db/libexec/bin:/opt/homebrew/opt/gnu-tar/libexec/gnubin:/opt/homebrew/opt/gawk/libexec/gnubin:/opt/homebrew/opt/gnu-which/libexec/gnubin:/opt/homebrew/opt/gnu-sed/libexec/gnubin:/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
 export PATH="$PATH:/Users/mik.dmitriev/Library/Application Support/Coursier/bin"
 export PATH="/opt/homebrew/opt/curl/bin:$PATH"
