ff423570a58adeeb12e451a7459ca95764f0494f
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-10-10 08:14:10 +0300

    remove(zsh): remove useless envs

--- zsh/mac.zsh
+++ zsh/mac.zsh
@@ -1,5 +1,4 @@
 export PNPM_HOME="$HOME/Library/pnpm"
-export ORACLE_HOME="/opt/oracle/instantclient_18_1"
 export PATH="opt/homebrew/Cellar/pyenv-virtualenv/1.1.5/shims:/opt/homebrew/bin:/opt/homebrew/sbin:/Library/Frameworks/Python.framework/Versions/3.10/bin:/Library/Frameworks/Python.framework/Versions/3.9/bin:/Library/Apple/usr/bin:/usr/local/opt/fzf/bin:$HOME/.gem/ruby/2.6.0/bin:$PNPM_HOME:$ORACLE_HOME:$PATH"
 export PATH="/opt/homebrew/opt/man-db/libexec/bin:/opt/homebrew/opt/gnu-tar/libexec/gnubin:/opt/homebrew/opt/gawk/libexec/gnubin:/opt/homebrew/opt/gnu-which/libexec/gnubin:/opt/homebrew/opt/gnu-sed/libexec/gnubin:/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
 export PATH="$PATH:/Users/mik.dmitriev/Library/Application Support/Coursier/bin"
@@ -9,4 +8,3 @@ export CPPFLAGS="-I/opt/homebrew/opt/curl/include"
 export HOMEBREW_AUTO_UPDATING=0
 alias xdg-open=open
 export HOMEBREW_CASK_OPTS="--no-quarantine"
-export DYLD_LIBRARY_PATH="$(brew --prefix)/lib:$DYLD_LIBRARY_PATH"
