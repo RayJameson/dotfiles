e32306ee32cdba9fdadda5d23be2363b0eb954d2
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-11-09 02:37:40 +0500

    fix(install): force linking

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ fi
 ##############################
 #           ZSH              #
 ##############################
-ln -s "$(pwd)/zsh/.zshrc" "$HOME/.zshrc"
-ln -s "$(pwd)/zsh/.zprofile" "$HOME/.zprofile"
+ln -sf "$(pwd)/zsh/.zshrc" "$HOME/.zshrc"
+ln -sf "$(pwd)/zsh/.zprofile" "$HOME/.zprofile"
 
 ##############################
 #           RDT              #
@@ -27,48 +27,48 @@ cd rdt && bash ./install.sh "$LOCAL_BIN" && cd - || exit 1
 ##############################
 #       POWERLEVEL10K        #
 ##############################
-ln -s "$(pwd)/zsh/powerlevel10k" "$(pwd)/zsh/oh-my-zsh/custom/themes"
+ln -sf "$(pwd)/zsh/powerlevel10k" "$(pwd)/zsh/oh-my-zsh/custom/themes"
 
 ##############################
 #           VI(M)            #
 ##############################
-ln -s "$(pwd)/vi-m/.vimrc" "$HOME"
-ln -s "$(pwd)/vi-m/.ideavimrc" "$HOME"
+ln -sf "$(pwd)/vi-m/.vimrc" "$HOME"
+ln -sf "$(pwd)/vi-m/.ideavimrc" "$HOME"
 
 ##############################
 #         ASTRONVIM          #
 ##############################
-ln -s "$(pwd)/nvim" "$CONFIG"
+ln -sf "$(pwd)/nvim" "$CONFIG"
 
 ##############################
 #     ASTRONVIM CONFIG       #
 ##############################
-ln -s "$(pwd)/nvim" "$CONFIG"
+ln -sf "$(pwd)/nvim" "$CONFIG"
 
 ##############################
 #          LAZYGIT           #
 ##############################
-ln -s "$(pwd)/lazygit" "$CONFIG"
+ln -sf "$(pwd)/lazygit" "$CONFIG"
 
 ##############################
 #          WEZTERM           #
 ##############################
-ln -s "$(pwd)/wezterm" "$CONFIG"
+ln -sf "$(pwd)/wezterm" "$CONFIG"
 
 ##############################
 #           TMUX             #
 ##############################
-ln -s "$(pwd)/tmux" "$CONFIG"
+ln -sf "$(pwd)/tmux" "$CONFIG"
 
 ##############################
 #         PTPYTHON           #
 ##############################
-ln -s "$(pwd)/ptpython" "$CONFIG"
+ln -sf "$(pwd)/ptpython" "$CONFIG"
 
 ##############################
 #          PYLINT            #
 ##############################
-ln -s "$(pwd)/pylintrc" "$CONFIG"
+ln -sf "$(pwd)/pylintrc" "$CONFIG"
 
 ##############################
 #            NVM             #
@@ -87,8 +87,8 @@ curl https://pyenv.run | bash && ln -s "$(pwd)/pyenv/default-packages" "$HOME/.p
 ##############################
 #            GIT             #
 ##############################
-ln -s "$(pwd)/git/.gitconfig" "$HOME"
-ln -s "$(pwd)/git/.gitignore_global" "$HOME"
+ln -sf "$(pwd)/git/.gitconfig" "$HOME"
+ln -sf "$(pwd)/git/.gitignore_global" "$HOME"
 
 ##############################
 #           ROFI             #
@@ -108,7 +108,7 @@ if [[ $(uname) == "Linux" ]]; then
 ##############################
 #             i3             #
 ##############################
-ln -s "$(pwd)/i3" "$CONFIG"
+ln -sf "$(pwd)/i3" "$CONFIG"
 fi
 
 
