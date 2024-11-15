8ef9ddfb568c997dc86cb99919b97ca9aa1bac3b
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-11-15 11:04:55 +0300

    fix(zshrc): ctrl-t ctrl-c shortcuts didn't work

--- dot_zshrc.d/10-exports.zsh
+++ dot_zshrc.d/10-exports.zsh
@@ -3,6 +3,21 @@ export HISTSIZE=10000000
 export SAVEHIST=10000000
 # Add colors to Terminal
 export LSCOLORS=ExFxBxDxCxegedabagacad
+if [[ $(command -v fd) ]] || [[ $(command -v fdfind) ]] || [[ $(command -v fd-find) ]]; then
+    export FZF_DEFAULT_COMMAND='fd --strip-cwd-prefix --hidden --follow --exclude .git'
+    # Use fd (https://github.com/sharkdp/fd) instead of the default find
+    # command for listing path candidates.
+    # - The first argument to the function ($1) is the base path to start traversal
+    # - See the source code (completion.{bash,zsh}) for the details.
+    _fzf_compgen_path() {
+        fd --hidden --follow --exclude ".git" . "$1"
+    }
+
+    # Use fd to generate the list for directory completion
+    _fzf_compgen_dir() {
+        fd --type d --hidden --follow --exclude ".git" . "$1"
+    }
+fi
 export FZF_TMUX_OPTS="-p80%,60% --color=dark --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f
   --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND -t=f -t=d"
