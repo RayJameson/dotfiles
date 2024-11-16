ab4e342f10b5e4c9b303e0437aa1bc46d4ca3f1f
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-11-17 04:48:37 +0500

    feat(nvim)!: auto apply on edit in `chezmoi` dir

--- dot_config/nvim/lua/plugins/chezmoi.lua
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -1,12 +1,24 @@
 ---@type LazySpec
 return {
-  "AstroNvim/astrocore",
-  opts = {
-    mappings = {
-      n = {
-        ["<Leader>f."] = {
-          function() require("telescope").extensions.chezmoi.find_files() end,
-          desc = "Find chezmoi config",
+  {
+    "xvzc/chezmoi.nvim",
+    optional = true,
+    opts = {
+      edit = {
+        watch = true,
+        force = false,
+      },
+    },
+  },
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          ["<Leader>f."] = {
+            function() require("telescope").extensions.chezmoi.find_files() end,
+            desc = "Find chezmoi config",
+          },
         },
       },
     },
