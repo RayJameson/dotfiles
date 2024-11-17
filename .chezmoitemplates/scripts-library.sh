aba56e157294ba7c75366c35bedac5c5c7f4ee68
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-11-17 10:25:40 +0300

    fix(chezmoi): use script to ask user input, not in config

---
+++ .chezmoitemplates/scripts-library.sh
@@ -0,0 +1,19 @@
+set -euo pipefail
+
+function ask_user() {
+    printf "$1 [y/n] "
+    read answer < /dev/tty
+
+    case $answer in
+        y|Y|yes)
+            return 0
+            ;;
+        n|N|no)
+            return 1
+            ;;
+        *)
+            echo "Unrecognized input"
+            exit 1
+            ;;
+    esac
+}
