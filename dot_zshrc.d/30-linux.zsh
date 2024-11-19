d3532c42257dc532350712c90afaed5f46f0d27e
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-11-20 03:22:18 +0500

    feat(zhsrc.d): use rofi_askpass for sudo password prompt

--- dot_zshrc.d/30-linux.zsh
+++ dot_zshrc.d/30-linux.zsh
@@ -6,5 +6,7 @@ function zvm_after_init() {
         source <(fzf --zsh)
     fi
 }
+export SUDO_ASKPASS="$HOME/.config/i3/scripts/rofi_askpass"
+alias sudo='sudo -A'
 
 command_exists stderred && export LD_PRELOAD="/usr/\$LIB/libstderred.so${LD_PRELOAD:+:$LD_PRELOAD}"
