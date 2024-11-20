da3673402764926006acaea5b439b8a0f34e4ac7
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-11-20 07:50:14 +0500

    feat(zshrc.d): prompt password using rofi for sudoedit

--- dot_zshrc.d/30-linux.zsh
+++ dot_zshrc.d/30-linux.zsh
@@ -8,6 +8,7 @@ function zvm_after_init() {
 }
 export SUDO_ASKPASS="$HOME/.config/i3/scripts/rofi_askpass"
 alias sudo='sudo -A'
+alias sudoedit='sudoedit -A'
 alias lrun='systemd-run --user --scope --property CPUQuota=80% --property MemoryMax=31G --property MemoryHigh=28G'
 alias lrunbg='systemd-run --user --property CPUQuota=80% --property MemoryMax=31G --property MemoryHigh=28G'
 
