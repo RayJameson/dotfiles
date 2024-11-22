4f026af4832a3e2ce65188739fb76aae8be61e90
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-11-22 17:09:53 +0300

    feat(nvim): readded flatten.nvim

---
+++ dot_config/nvim/lua/plugins/flatten.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+  "willothy/flatten.nvim",
+  lazy = false,
+  priority = 99999,
+  opts = {
+    window = {
+      open = "alternate",
+    },
+    nest_if_no_args = true,
+  },
+}
