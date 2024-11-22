a24ae9912bd3477835266efcf4ecc8d4ec7ffc22
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-11-22 16:41:09 +0300

    feat(nvim): added keybinding for yazi in toggleterm

--- dot_config/nvim/lua/plugins/toggleterm.lua
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -6,8 +6,26 @@ return {
     direction = "float",
     autochdir = true,
     float_opts = {
-      width = function() return require("utilities").calculate_percent(vim.api.nvim_win_get_width(0), 95) end,
-      height = function() return require("utilities").calculate_percent(vim.api.nvim_win_get_height(0), 95) end,
+      width = function()
+        return require("utilities").calculate_percent(vim.api.nvim_win_get_width(0), 95)
+      end,
+      height = function()
+        return require("utilities").calculate_percent(vim.api.nvim_win_get_height(0), 95)
+      end,
     },
+  },
+  specs = {
+    "AstroNvim/astrocore",
+    opts = function(_, opts)
+      local maps = opts.mappings
+      if vim.fn.executable("yazi") == 1 then
+        maps.n["<Leader>ty"] = {
+          function()
+            require("astrocore").toggle_term_cmd("yazi")
+          end,
+          desc = "ToggleTerm yazi",
+        }
+      end
+    end,
   },
 }
