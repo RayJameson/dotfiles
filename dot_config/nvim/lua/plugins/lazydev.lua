a4f8a1860ef9ba4e370c8d621537efe9253cd26e
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-11-24 09:29:33 +0500

    fix(nvim): lazydev extend library

--- dot_config/nvim/lua/plugins/lazydev.lua
+++ dot_config/nvim/lua/plugins/lazydev.lua
@@ -3,6 +3,7 @@ return {
   "folke/lazydev.nvim",
   optional = true,
   specs = { "justinsgithub/wezterm-types", lazy = true },
+  opts_extend = { "library" },
   opts = {
     library = {
       { path = "wezterm-types/types", mods = { "wezterm" } },
