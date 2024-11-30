581c6954d2f2198056463aa74192cb9a0425858e
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-11-30 22:29:23 +0300

    refactor(less): move lessfilter to config dir

--- dot_zshrc.d/10-options.zsh
+++ dot_zshrc.d/10-options.zsh
@@ -10,7 +10,7 @@ zstyle ':fzf-tab:*' fzf-flags --bind=ctrl-u:preview-up,ctrl-d:preview-down --bin
   --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f \
   --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7
 zstyle ':fzf-tab:complete:*:*' fzf-preview 'less ${(Q)realpath}'
-export LESSOPEN='|~/.lessfilter %s'
+export LESSOPEN='|~/.config/less/.lessfilter %s'
 zstyle ':fzf-tab:complete:*:options' fzf-preview 
 zstyle ':fzf-tab:complete:*:argument-1' fzf-preview
 
