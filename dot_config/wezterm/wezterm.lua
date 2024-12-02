d4dbc4873e5af222c3dd4ae41ba2159ff98e3003
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-12-02 23:04:35 +0500

    fix(wezterm): font wasn't recognised correctly
    resulted in bold italics

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -11,7 +11,7 @@ end
 local transparency_value = 0.7
 config.window_background_opacity = wezterm.GLOBAL.window_background_opacity or transparency_value
 -- This is where you actually apply your config choices
-wezterm.on("toggle-transparency", function(window, _)
+wezterm.on("toggle-transparency", function()
   if wezterm.GLOBAL.window_background_opacity == transparency_value or wezterm.GLOBAL.window_background_opacity == nil then
     wezterm.GLOBAL.window_background_opacity = 1
   elseif wezterm.GLOBAL.window_background_opacity == 1 then
@@ -19,41 +19,44 @@ wezterm.on("toggle-transparency", function(window, _)
   end
   wezterm.reload_configuration()
 end)
-local font_name
 -- Check current OS and apply background effects
-font_name = "Iosevka Nerd Font Mono Condensed ExtraLight"
+---@type FontAttributes
+---@diagnostic disable-next-line: missing-fields
+local font_attributes = {
+  family = "Iosevka Nerd Font Mono",
+  weight = "ExtraLight",
+  stretch = "Condensed",
+  freetype_load_flags = "NO_HINTING",
+}
 if wezterm.target_triple:match("darwin") then
   config.macos_window_background_blur = 30
   config.font_size = 24.5
 elseif wezterm.target_triple:match("windows") then
+  ---@diagnostic disable-next-line: assign-type-mismatch
   config.win32_system_backdrop = "Acrylic"
   config.term = "" -- Set to empty so FZF works on windows
   config.default_prog = { "pwsh.exe" }
 elseif wezterm.target_triple:match("linux") then
+  font_attributes.scale = 0.92
   config.font_size = 20
 end
 
--- config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
 config.tab_max_width = 25
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
 
--- config.harfbuzz_features = { "zero" } -- zero with dot instead of slashed zero
--- local font_name = "LigaMesloLGSDZ Nerd Font Mono"
---
-config.font = wezterm.font(font_name)
-if font_name:match("[Nn]erd") then
+---@diagnostic disable-next-line: param-type-mismatch, redundant-parameter, assign-type-mismatch
+config.font = wezterm.font(font_attributes)
+if font_attributes.family:match("[Nn]erd") then
   config.set_environment_variables = {
-    NERD_FONT = font_name,
+    NERD_FONT = font_attributes.family,
   }
 end
 --<!-- -- != := === == != >= >- >=> |-> -> <$> </> #[ |||> |= ~@
 
-config.freetype_load_flags = "NO_HINTING"
-
--- For example, changing the color scheme:
 config.color_scheme = "Dracula (Official)"
+---@diagnostic disable-next-line: missing-fields
 config.colors = {
   visual_bell = "grey",
   background = "#1e222a",
@@ -68,6 +71,7 @@ config.window_padding = {
   bottom = 0,
 }
 
+---@diagnostic disable-next-line: assign-type-mismatch
 config.audible_bell = "Disabled"
 config.visual_bell = {
   fade_in_function = "EaseIn",
