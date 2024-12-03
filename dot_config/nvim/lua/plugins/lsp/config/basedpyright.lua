5ad0bcd6cdfc0c4e60545fe2a34fff18a20b15b5
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-12-03 14:45:29 +0300

    fix(nvim): python decorators has same priority as functions

--- dot_config/nvim/lua/plugins/lsp/config/basedpyright.lua
+++ dot_config/nvim/lua/plugins/lsp/config/basedpyright.lua
@@ -12,6 +12,22 @@ return {
           c.settings.python.pythonPath = (vim.env.VIRTUAL_ENV and vim.env.VIRTUAL_ENV .. "/bin/python3")
             or vim.fn.exepath("python3")
         end,
+        on_attach = function()
+          vim.api.nvim_create_autocmd("LspTokenUpdate", {
+            callback = function(args)
+              local token = args.data.token
+              if token.type == "decorator" and not token.modifiers.readonly then
+                vim.lsp.semantic_tokens.highlight_token(
+                  token,
+                  args.buf,
+                  args.data.client_id,
+                  "@lsp.type.decorator.python",
+                  { priority = 130 }
+                )
+              end
+            end,
+          })
+        end,
         settings = {
           basedpyright = {
             analysis = {
