4034b281c0d951372d4dc858a1dd6bf98fb94c92
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-12-07 07:45:53 +0300

    refactor(zshrc.d): darwin path use homebrew for prefix

--- dot_zshrc.d/20-darwin.zsh
+++ dot_zshrc.d/20-darwin.zsh
@@ -5,14 +5,27 @@ if [[ -d "$HOME/.terminfo" ]]; then
     export TERMINFO="$HOME/.terminfo" # this saves your santity on MacOS if you are using TMUX
 fi
 export PNPM_HOME="$HOME/Library/pnpm"
-export PATH="opt/homebrew/Cellar/pyenv-virtualenv/1.1.5/shims:/opt/homebrew/bin:/opt/homebrew/sbin:/Library/Frameworks/Python.framework/Versions/3.10/bin:/Library/Frameworks/Python.framework/Versions/3.9/bin:/Library/Apple/usr/bin:/usr/local/opt/fzf/bin:$HOME/.gem/ruby/2.6.0/bin:$PNPM_HOME:$ORACLE_HOME:$PATH"
-export PATH="/opt/homebrew/opt/man-db/libexec/bin:/opt/homebrew/opt/gnu-tar/libexec/gnubin:/opt/homebrew/opt/gawk/libexec/gnubin:/opt/homebrew/opt/gnu-which/libexec/gnubin:/opt/homebrew/opt/gnu-sed/libexec/gnubin:/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
-export PATH="$PATH:/Users/mik.dmitriev/Library/Application Support/Coursier/bin"
-export PATH="/opt/homebrew/opt/curl/bin:$PATH"
-export LDFLAGS="-L/opt/homebrew/opt/curl/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/curl/include"
-export HOMEBREW_AUTO_UPDATING=0
-alias xdg-open=open
-export HOMEBREW_CASK_OPTS="--no-quarantine"
-zstyle ':fzf-tab:complete:brew-(install|uninstall|search|info):*-argument-rest' fzf-preview 'brew info $word'
+if command_exists brew; then
+    export HOMEBREW_CASK_OPTS="--no-quarantine"
+    export HOMEBREW_AUTO_UPDATING=0
+    export HOMEBREW_PREFIX="$(brew --prefix)"
+    export LDFLAGS="-L$HOMEBREW_PREFIX/opt/curl/lib"
+    export CPPFLAGS="-I$HOMEBREW_PREFIX/opt/curl/include"
+    PATH="$HOMEBREW_PREFIX/Cellar/pyenv-virtualenv/1.1.5/shims/$HOMEBREW_PREFIX/bin:$PATH"
+    PATH="$HOMEBREW_PREFIX/sbin:$PATH"
+    PATH="$HOMEBREW_PREFIX/opt/curl/bin:$PATH"
+    PATH="$HOMEBREW_PREFIX/opt/man-db/libexec/bin:$PATH"
+    PATH="$HOMEBREW_PREFIX/opt/gnu-tar/libexec/gnubin:$PATH"
+    PATH="$HOMEBREW_PREFIX/opt/gawk/libexec/gnubin:$PATH"
+    PATH="$HOMEBREW_PREFIX/opt/gnu-which/libexec/gnubin:$PATH"
+    PATH="$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin:$PATH"
+    PATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:$PATH"
+fi
+PATH="/Library/Frameworks/Python.framework/Versions/3.10/bin:/Library/Frameworks/Python.framework/Versions/3.9/bin:/Library/Apple/usr/bin:/usr/local/opt/fzf/bin:$HOME/.gem/ruby/2.6.0/bin:$PNPM_HOME:$ORACLE_HOME:$PATH"
+PATH="$PATH:/Users/mik.dmitriev/Library/Application Support/Coursier/bin"
 command_exists ggrep && alias grep="ggrep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}"
+alias xdg-open=open
+zstyle ':fzf-tab:complete:brew-(install|uninstall|search|info):*-argument-rest' fzf-preview 'brew info $word'
+
+typeset -U PATH path
+export PATH
