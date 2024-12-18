3391cadc6c634982a3a854641bd035e315f2a56e
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-12-18 09:14:28 +0300

    feat(nvim): use horizontal split term by default

--- dot_config/nvim/lua/plugins/toggleterm.lua
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
   "akinsho/toggleterm.nvim",
   optional = true,
   opts = {
-    direction = "float",
+    direction = "horizontal",
     autochdir = true,
     float_opts = {
       width = function() return require("utilities").calculate_percent(vim.api.nvim_win_get_width(0), 95) end,
@@ -16,7 +16,7 @@ return {
       local maps = opts.mappings
       if vim.fn.executable("yazi") == 1 then
         maps.n["<Leader>ty"] = {
-          function() require("astrocore").toggle_term_cmd("yazi") end,
+          function() require("astrocore").toggle_term_cmd{ cmd = "yazi", direction = "float" } end,
           desc = "ToggleTerm yazi",
         }
       end
