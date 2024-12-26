ebc5a97c8adf0f420d4780ae2007aac577d2443b
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-12-26 16:26:12 +0300

    chore(nvim): render-markdown - simplify ft and remove border around code
    better for transparent mode

--- dot_config/nvim/lua/plugins/render-markdown.lua
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -1,11 +1,7 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
   cmd = "RenderMarkdown",
-  ft = function()
-    local plugin = require("lazy.core.config").spec.plugins["render-markdown.nvim"]
-    local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-    return opts.file_types or { "markdown" }
-  end,
+  ft = "markdown",
   dependencies = {
     {
       "nvim-treesitter/nvim-treesitter",
@@ -29,6 +25,9 @@ return {
     },
     anti_conceal = {
       enabled = false,
+    },
+    code = {
+      border = false,
     },
   },
 }
