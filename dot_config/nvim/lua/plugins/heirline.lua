f6bf37fe5273d9d9f5e73ecd09ba88ab1757954a
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2025-01-17 11:54:08 +0300

    feat(nvim): heirline update dap status in real time

--- dot_config/nvim/lua/plugins/heirline.lua
+++ dot_config/nvim/lua/plugins/heirline.lua
@@ -21,10 +21,15 @@ return {
           },
           provider = function(self)
             local mapped_debugger_status = require("dap").session().stopped_thread_id == nil and "RUNNING" or "STOPPED"
-            self.color = self.colors[mapped_debugger_status]
+            vim.g.dap_debugger_last_status = mapped_debugger_status
             return self.symbols[mapped_debugger_status]
           end,
           hl = function(self) return { fg = self.color } end,
+          update = function(self)
+            local mapped_debugger_status = require("dap").session().stopped_thread_id == nil and "RUNNING" or "STOPPED"
+            self.color = self.colors[mapped_debugger_status]
+            return mapped_debugger_status ~= vim.g.dap_debugger_last_status
+          end,
         },
       }
     end
