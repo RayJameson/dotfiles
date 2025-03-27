0d4366975041b37a1336043271254084268ce8f6
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2025-03-27 14:53:46 +0300

    feat(nvim,blink.cmp): disable ghost text

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -2,4 +2,14 @@
 return {
   "Saghen/blink.cmp",
   version = "*",
+  opts = {
+    completion = {
+      ghost_text = {
+        enabled = false,
+        show_with_menu = false,
+        show_with_selection = false,
+        show_without_menu = false,
+      },
+    },
+  },
 }
