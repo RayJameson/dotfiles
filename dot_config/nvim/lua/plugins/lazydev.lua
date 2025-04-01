8491cdb712f0e7ee5ca3d5c9bf6c1eeea19847a1
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2025-04-01 05:35:46 +0500

    remove(nvim,lazydev): remove wezterm types since i'm using ghostty

--- dot_config/nvim/lua/plugins/lazydev.lua
+++ dot_config/nvim/lua/plugins/lazydev.lua
@@ -2,11 +2,5 @@
 return {
   "folke/lazydev.nvim",
   optional = true,
-  specs = { "justinsgithub/wezterm-types", lazy = true },
-  opts_extend = { "library" },
-  opts = {
-    library = {
-      { path = "wezterm-types/types", mods = { "wezterm" } },
-    },
-  },
+  opts = {},
 }
