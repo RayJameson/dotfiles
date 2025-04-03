129d605901bb1686cbc32e79f17f54a376be3289
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2025-04-03 17:52:56 +0300

    remove(nvim,dap): lua vscode debugger

--- dot_config/nvim/lua/plugins/dap.lua
+++ dot_config/nvim/lua/plugins/dap.lua
@@ -6,41 +6,6 @@ return {
   -- defaults write com.apple.CrashReporter DialogType prompt
   -- credit: https://github.com/mfussenegger/nvim-dap-python/issues/151#issuecomment-2221639990
   "mfussenegger/nvim-dap",
-  config = function()
-    local dap = require("dap")
-    dap.adapters["local-lua"] = {
-      type = "executable",
-      command = "node",
-      args = {
-        vim.env.MASON .. "/share/local-lua-debugger-vscode/extension/debugAdapter.js",
-      },
-      enrich_config = function(config, on_config)
-        if not config["extensionPath"] then
-          local c = vim.deepcopy(config)
-          -- 💀 If this is missing or wrong you'll see
-          -- "module 'lldebugger' not found" errors in the dap-repl when trying to launch a debug session
-          c.extensionPath = vim.env.MASON .. "/share/local-lua-debugger-vscode/"
-          on_config(c)
-        else
-          on_config(config)
-        end
-      end,
-    }
-    dap.configurations.lua = {
-      {
-        name = "Current file (local-lua-dbg, lua)",
-        type = "local-lua",
-        request = "launch",
-        cwd = "${workspaceFolder}",
-        program = {
-          lua = "lua5.1",
-          file = "${file}",
-        },
-        args = {},
-      },
-    }
-    require("astronvim.plugins.configs.nvim-dap")()
-  end,
   specs = {
     { "AstroNvim/astroui", opts = { icons = { Debugger = "" } } },
     {
