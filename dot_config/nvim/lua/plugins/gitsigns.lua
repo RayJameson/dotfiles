cdd2ddb949b016017a424d6e4745fc1349d5d088
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2025-04-10 13:05:51 +0300

    chore(nvim,gitsigns): use newer api

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -6,7 +6,7 @@ return {
     { prefix .. "P", function() require("gitsigns").preview_hunk() end, desc = "Preview Git hunk in pop up" },
     { prefix .. "R", function() require("gitsigns").reset_buffer() end, desc = "Reset Git buffer" },
     { prefix .. "r", function() require("gitsigns").reset_hunk() end, desc = "Reset Git hunk" },
-    { prefix .. "D", function() require("gitsigns").toggle_deleted() end, desc = "Toggle deleted lines" },
+    { prefix .. "D", function() require("gitsigns").preview_hunk_inline() end, desc = "Toggle deleted lines" },
     { prefix .. "w", function() require("gitsigns").toggle_word_diff() end, desc = "Toggle word diff" },
   },
 }
