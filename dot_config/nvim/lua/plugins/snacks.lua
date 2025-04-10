8fab01b10b15061ed697e2380e797325b41df47d
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2025-04-10 16:44:40 +0300

    feat(nvim,snacks): disable statuscolumn in snacks

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -10,7 +10,7 @@ return {
     quickfile = {},
     words = {},
     notifier = {},
-    statuscolumn = {},
+    statuscolumn = { enabled = false },
     picker = {
       layouts = {
         custom_ivy_split = (function()
