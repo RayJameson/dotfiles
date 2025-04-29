86f58e69b0e71b63b41952162b8625ae662b0d95
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2025-04-29 10:20:07 +0300

    chore(nvim,lsp): disable rules for commas

--- dot_config/nvim/lua/plugins/lsp/config/ruff.lua
+++ dot_config/nvim/lua/plugins/lsp/config/ruff.lua
@@ -10,7 +10,7 @@ return {
           settings = {
             args = {
               "--select=B,E,F,I,ANN,ASYNC,S,COM,C,ISC,PIE,PT,Q,TID,ARG,PLE,PLR,PLW,RUF",
-              "--ignore=ANN101,RUF100,PLR0913,ANN102,ANN401,PLR2004,S101",
+              "--ignore=ANN101,RUF100,PLR0913,ANN102,ANN401,PLR2004,S101,COM812,COM819",
               "--line-length=120",
               "--fixable=A,B,C,D,E,F,G,I,N,Q,S,T,W,ANN,ARG,BLE,COM,DJ,DTZ,EM,ERA,EXE,FBT,ICN,INP,ISC,NPY,PD,PGH,PIE,PL,PT,PTH,PYI,RET,RSE,RUF,SIM,SLF,TCH,TID,TRY,UP,YTT",
             },
