791f81f2323152e8ca64336031e9b0967cf99559
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2025-05-01 11:05:18 +0500

    feat(nvim,polish): add filetypes

--- dot_config/nvim/lua/polish.lua
+++ dot_config/nvim/lua/polish.lua
@@ -77,6 +77,8 @@ vim.filetype.add {
     ["%.gitconfig.*"] = "gitconfig",
     [".*/%.vscode/.+%.json"] = "jsonc",
     [".*/i3/config%.d/.+%.conf"] = "i3config",
+    [".*/hypr/**/.+%.conf"] = "hyprlang",
+    [".*/uwsm/env"] = "sh",
   },
 }
 
