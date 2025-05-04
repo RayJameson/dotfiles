796e777c60a1ad3651b32875c85731273fcb664c
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2025-05-04 10:42:33 +0500

    fix(waybar): mic mute on click

---
+++ dot_config/hypr/scripts/executable_volume.sh
@@ -0,0 +1,32 @@
+toggle_mic() {
+	if [ "$(pamixer --default-source --get-mute)" == "false" ]; then
+		pamixer --default-source -m
+	elif [ "$(pamixer --default-source --get-mute)" == "true" ]; then
+		pamixer -u --default-source u
+	fi
+}
+
+inc_mic_volume() {
+    if [ "$(pamixer --default-source --get-mute)" == "true" ]; then
+        toggle_mic
+    else
+        pamixer --default-source -i 1
+    fi
+}
+
+dec_mic_volume() {
+    if [ "$(pamixer --default-source --get-mute)" == "true" ]; then
+        toggle-mic
+    else
+        pamixer --default-source -d 1
+    fi
+}
+
+
+if [[ "$1" == "--toggle-mic" ]]; then
+	toggle_mic
+elif [[ "$1" == "--mic-inc" ]]; then
+	inc_mic_volume
+elif [[ "$1" == "--mic-dec" ]]; then
+	dec_mic_volume
+fi
