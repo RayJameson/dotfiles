88e019891123c276f538979b83ac4ab2b6b4c1fa
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2025-05-13 22:20:36 +0500

    Revert "fix(nvim,mason) migrate to v2 api"
    This reverts commit 102b700dc6bd189ddf73e0c16189c301dd7a960a.

--- dot_config/nvim/lua/custom-registry/packages/pylance.lua
+++ dot_config/nvim/lua/custom-registry/packages/pylance.lua
@@ -1,34 +1,30 @@
-return {
-  schema = "registry+v1",
+local Pkg = require("mason-core.package")
+return Pkg.new {
   name = "pylance",
-  description = [[Fast, feature-rich language support for Python]],
+  desc = [[Fast, feature-rich language support for Python]],
   homepage = "https://github.com/microsoft/pylance",
-  licenses = { "proprietary" },
-  languages = { "Python" },
-  categories = { "LSP" },
-  source = {
-    id = "pkg:lua/pylance",
-    ---@param ctx InstallContext
-    install = function(ctx)
-      ctx.spawn.bash {
-        "-c",
-        ("curl -o pylance.zip https://ms-python.gallery.vsassets.io/_apis/public/gallery/publisher/ms-python/extension/vscode-pylance/%s/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"):format(
-          ctx.opts.version
-        ),
-      }
-      ctx.spawn.unzip { "pylance.zip" }
-      ctx.spawn.rm { "pylance.zip" }
-      ctx.spawn.bash {
-        "-c",
-        [[awk 'BEGIN{RS=ORS=";"} /if\(!process/ && !found {sub(/return!0x1/, "return!0x0"); found=1} 1' extension/dist/server.bundle.js | awk 'BEGIN{RS=ORS=";"} /throw new/ && !found {sub(/throw new/, ""); found=1} 1' > extension/dist/server_crack.js]],
-      }
-      ctx:link_bin(
+  languages = { Pkg.Lang.Python },
+  categories = { Pkg.Cat.LSP },
+  install = function(ctx)
+    ctx.receipt:with_primary_source(ctx.receipt.unmanaged)
+    ctx.spawn.bash {
+      "-c",
+      ("curl -o pylance.zip https://ms-python.gallery.vsassets.io/_apis/public/gallery/publisher/ms-python/extension/vscode-pylance/%s/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"):format(
+        ctx.requested_version:or_else("latest")
+      ),
+    }
+    ctx.spawn.unzip { "pylance.zip" }
+    ctx.spawn.rm { "pylance.zip" }
+    ctx.spawn.bash {
+      "-c",
+      [[awk 'BEGIN{RS=ORS=";"} /if\(!process/ && !found {sub(/return!0x1/, "return!0x0"); found=1} 1' extension/dist/server.bundle.js | awk 'BEGIN{RS=ORS=";"} /throw new/ && !found {sub(/throw new/, ""); found=1} 1' > extension/dist/server_crack.js]],
+    }
+    ctx:link_bin(
+      "pylance",
+      ctx:write_node_exec_wrapper(
         "pylance",
-        ctx:write_node_exec_wrapper(
-          "pylance",
-          require("mason-core.path").concat { "extension", "dist", "server_crack.js" }
-        )
+        require("mason-core.path").concat { "extension", "dist", "server_crack.js" }
       )
-    end,
-  },
+    )
+  end,
 }
--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -40,6 +40,7 @@ return {
   },
   {
     "mason-org/mason.nvim",
+    version = "1.11.0",
     opts = {
       registries = {
         "lua:custom-registry", -- custom user registry
