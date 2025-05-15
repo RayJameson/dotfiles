6999e0f64e4e40a4fa995835adab331ba8a29a01
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2025-05-15 12:21:32 +0500

    remove(nvim,mason): custom registry

--- dot_config/nvim/lua/custom-registry/init.lua
+++
@@ -1,4 +0,0 @@
-return {
-  pylance = "custom-registry.packages.pylance",
-  systemd_ls = "custom-registry.packages.systemd_ls",
-}
--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -42,9 +42,6 @@ return {
     "mason-org/mason.nvim",
     version = "1.11.0",
     opts = {
-      registries = {
-        "lua:custom-registry", -- custom user registry
-      },
       ui = {
         border = "rounded",
         height = 0.8,
