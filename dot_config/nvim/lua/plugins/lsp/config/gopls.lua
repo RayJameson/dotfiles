57ce1dce4757f588e2cd795b19fa0a212b2806fe
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2025-07-06 01:44:42 +0500

    feat(nvim,lsp,gopls): disable completeFunctionCalls

--- dot_config/nvim/lua/plugins/lsp/config/gopls.lua
+++ dot_config/nvim/lua/plugins/lsp/config/gopls.lua
@@ -8,6 +8,7 @@ return {
       gopls = {
         settings = {
           gopls = {
+            completeFunctionCalls = false,
             codelenses = {
               generate = true, -- show the `go generate` lens.
               gc_details = true, -- Show a code lens toggling the display of gc's choices.
