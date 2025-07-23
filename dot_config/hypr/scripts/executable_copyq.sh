66bad2a9918bc80686d3fd2724eb9ea0b9208fb9
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2025-07-23 07:57:54 +0500

    fix(hypr,scripts,copyq): wrong naming for width/height, used x/y

--- dot_config/hypr/scripts/executable_copyq.sh
+++ dot_config/hypr/scripts/executable_copyq.sh
@@ -15,18 +15,18 @@ active_monitor_name=$(echo "$active_monitor" | jq -r '.name')
 waybar_properties=$(hyprctl -j layers | jq -r ".\"$active_monitor_name\".levels | map(select(.[].namespace == \"waybar\")) | .[].[]")
 waybar_height=$(echo "$waybar_properties" | jq -r '.h')
 
-copyq_x_size=672
-copyq_y_size=702
+copyq_width=672
+copyq_height=702
 
-if [[ $(( desired_x + copyq_x_size )) -gt active_monitor_width ]]; then
-    desired_x=$(( active_monitor_width - copyq_x_size ))
-elif [[ $(( desired_x + copyq_x_size )) -lt active_monitor_width ]]; then
+if [[ $(( desired_x + copyq_width )) -gt active_monitor_width ]]; then
+    desired_x=$(( active_monitor_width - copyq_width ))
+elif [[ $(( desired_x + copyq_width )) -lt active_monitor_width ]]; then
     desired_x=$x
 fi
 
-if [[ $(( desired_y + copyq_y_size )) -gt active_monitor_height ]]; then
-    desired_y=$(( active_monitor_height - copyq_y_size ))
-elif [[ $(( desired_y + copyq_y_size )) -lt active_monitor_height ]]; then
+if [[ $(( desired_y + copyq_height )) -gt active_monitor_height ]]; then
+    desired_y=$(( active_monitor_height - copyq_height ))
+elif [[ $(( desired_y + copyq_height )) -lt active_monitor_height ]]; then
     desired_y=$(( y + waybar_height ))
 fi
 
