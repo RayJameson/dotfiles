fb592e000ffd7b34eaa9951042168a7f50ce928d
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2025-08-07 22:56:49 +0300

    feat(nvim,mason): pull out config from astrocommunity

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -1,3 +1,10 @@
+-- helper function to setup a plugin without an `ensure_installed` table
+local setup_without_ensure_installed = function(main, opts)
+  opts = vim.deepcopy(opts)
+  opts.ensure_installed = nil
+  require(main).setup(opts)
+end
+
 ---@return fun(): boolean
 local function is_executable(binary)
   return function() return vim.fn.executable(binary) == 1 end
@@ -10,7 +17,36 @@ local list_insert_unique = require("astrocore").list_insert_unique
 return {
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
+    init = function(plugin) require("astrocore").on_load("mason.nvim", plugin.name) end,
+    opts_extend = { "ensure_installed" },
+    config = function(_, opts)
+      local mason_tool_installer = require("mason-tool-installer")
+      mason_tool_installer.setup(opts)
+      if opts.run_on_start ~= false then mason_tool_installer.run_on_start() end
+    end,
+    specs = {
+      -- disable init and ensure installed of other plugins
+      {
+        "jay-babu/mason-nvim-dap.nvim",
+        optional = true,
+        init = false,
+        config = function(_, opts) setup_without_ensure_installed("mason-nvim-dap", opts) end,
+      },
+      {
+        "williamboman/mason-lspconfig.nvim",
+        optional = true,
+        init = false,
+        config = function(_, opts) setup_without_ensure_installed("mason-lspconfig", opts) end,
+      },
+      {
+        "jay-babu/mason-null-ls.nvim",
+        optional = true,
+        init = false,
+        config = function(_, opts) setup_without_ensure_installed("mason-null-ls", opts) end,
+      },
+    },
     opts = {
+      integrations = { ["mason-lspconfig"] = false, ["mason-null-ls"] = false, ["mason-nvim-dap"] = false },
       ensure_installed = {
         unpack(
           vim
