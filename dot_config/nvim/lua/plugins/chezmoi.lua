6b63aa21c9f71c986c82e1a255b3324f14ea45af
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2025-12-31 08:24:58 +0500

    fix(nvim,chezmoi): remove disable hl from treesitter, add mini icons

--- dot_config/nvim/lua/plugins/chezmoi.lua
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -15,12 +15,23 @@ return {
       },
     },
     {
-      "nvim-treesitter/nvim-treesitter",
-      opts = function(_, opts)
-        opts.highlight.disable = function(_, bufnr)
-          if vim.bo[bufnr].filetype == "chezmoitmpl" then return true end
-        end
-      end,
+      "echasnovski/mini.icons",
+      optional = true,
+      opts = {
+        file = {
+          [".chezmoiignore"] = { glyph = "", hl = "MiniIconsGrey" },
+          [".chezmoiremove"] = { glyph = "", hl = "MiniIconsGrey" },
+          [".chezmoiroot"] = { glyph = "", hl = "MiniIconsGrey" },
+          [".chezmoiversion"] = { glyph = "", hl = "MiniIconsGrey" },
+          ["bash.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+          ["json.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+          ["ps1.tmpl"] = { glyph = "󰨊", hl = "MiniIconsGrey" },
+          ["sh.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+          ["toml.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+          ["yaml.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+          ["zsh.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+        },
+      },
     },
   },
 }
