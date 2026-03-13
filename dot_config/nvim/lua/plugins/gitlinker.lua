770e68aee934b2e08429cb5b266c5dab00a2bfd9
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2026-03-13 09:20:35 +0500

    refactor(nvim,gitlinker): use astrocore instead of lazy's keys, fix icon display

--- dot_config/nvim/lua/plugins/gitlinker.lua
+++ dot_config/nvim/lua/plugins/gitlinker.lua
@@ -14,11 +14,22 @@ return {
     }
   end,
   cmd = "GitLink",
-  keys = function()
-    local prefix = "<Leader>g"
-    return {
-      { prefix .. "o", "<cmd>GitLink!<CR>", desc = "Open git line(s) in web", mode = { "n", "x" } },
-      { prefix .. "O", "<cmd>GitLink<CR>", desc = "Copy git line(s) url", mode = { "n", "x" } },
-    }
-  end,
+  specs = {
+    {
+      "AstroNvim/astrocore",
+      ---@param opts AstroCoreOpts
+      opts = function(_, opts)
+        if opts.mappings == nil then opts.mappings = { n = {}, x = {} } end
+        local maps, prefix = opts.mappings, "<Leader>g" ---@cast maps -nil
+        local icon = require("astroui").get_icon("Git", 1, true)
+        maps.n[prefix] = { desc = icon .. "Git" }
+        maps.x[prefix] = { desc = icon .. "Git" }
+        for _, mode in pairs { "n", "x" } do
+          maps[mode][prefix .. "o"] =
+            { function() vim.cmd.GitLink { bang = true } end, desc = "Open git line(s) in web" }
+          maps[mode][prefix .. "O"] = { function() vim.cmd.GitLink() end, desc = "Copy git line(s) url" }
+        end
+      end,
+    },
+  },
 }
